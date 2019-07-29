ffeb43b46f1c98e7347981fb0d94339fcc8c8fbe
prai <prai93@outlook.com>
2019-07-29 15:55:36 +0200

    started nignx testing

--- faas-gateway/src/app/app.go
+++ faas-gateway/src/app/app.go
@@ -2,18 +2,16 @@ package app
 
 import (
 	"fmt"
-	"io"
 
 	// local packages
 
 	// vendor packages
 	"github.com/docker/docker/client"
 	"github.com/docker/docker/api/types/container"
-	"os"
 
 	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/pkg/stdcopy"
 	"golang.org/x/net/context"
+	"github.com/docker/go-connections/nat"
 )
 
 func RunApp() {
@@ -22,44 +20,36 @@ func RunApp() {
 }
 
 func main() {
+
 	ctx := context.Background()
 	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
 		panic(err)
 	}
 
-	reader, err := cli.ImagePull(ctx, "docker.io/library/alpine", types.ImagePullOptions{})
+	hostBinding := nat.PortBinding{
+		HostIP:   "127.0.0.0",
+		HostPort: "8000",
+	}
+	containerPort, err := nat.NewPort("tcp", "80")
 	if err != nil {
-		panic(err)
-	}
-	io.Copy(os.Stdout, reader)
-
-	resp, err := cli.ContainerCreate(ctx, &container.Config{
-		Image: "alpine",
-		Cmd:   []string{"echo", "hello world"},
-		Tty:   true,
-	}, nil, nil, "")
-	if err != nil {
-		panic(err)
+		panic("Unable to get the port")
 	}
 
-	if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
-		panic(err)
-	}
+	portBinding := nat.PortMap{containerPort: []nat.PortBinding{hostBinding}}
 
-	statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
-	select {
-	case err := <-errCh:
-		if err != nil {
-			panic(err)
-		}
-	case <-statusCh:
-	}
-
-	out, err := cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true})
+	cont, err := cli.ContainerCreate(ctx, &container.Config{
+		Image: "nginx",
+	}, &container.HostConfig{
+			PortBindings: portBinding,
+		}, nil, "")
 	if err != nil {
 		panic(err)
 	}
 
-	stdcopy.StdCopy(os.Stdout, os.Stderr, out)
+
+	cli.ContainerStart(context.Background(), cont.ID, types.ContainerStartOptions{})
+	fmt.Printf("Container %s is started", cont.ID)
+
+
 }
